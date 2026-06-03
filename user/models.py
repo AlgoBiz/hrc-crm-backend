e421979ab654870303c7c85d9283ded3f91fd93d
GitHub Name <GitHub email>
2026-06-03 10:57:21 +0530

    Change User model's center ForeignKey on_delete behavior to CASCADE

--- user/models.py
+++ user/models.py
@@ -26,7 +26,7 @@ class User(AbstractUser):
         ('branch_user', 'Branch User'),
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='branch_user')
-    center = models.ForeignKey('Center', on_delete=models.SET_NULL, null=True, blank=True)
+    center = models.ForeignKey('Center', on_delete=models.CASCADE, null=True, blank=True)
     def __str__(self):
         return self.username
 
